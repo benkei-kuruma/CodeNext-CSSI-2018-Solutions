55c2aed0715df66a03bad7f4596ec0b1cb73aefc
ahob85 <aaron@ahob85.com>
2018-06-06 05:20:43 -0700

    update

---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log("It's a beautiful day for programming, isn't it?");
+console.log("Hey! What's 1 + 1?");
+console.log("It's " + 1 + 1 + " of course!");
+console.log("No, it's " + (1 + 1) + ", silly goose!");
+// console.log("This isn't going to get printed, is it?");
