c712ea7bdaad7320a3ec9c681d4de794e892679c
ahob85 <aaron@ahob85.com>
2018-06-15 22:54:30 -0700

    update

--- conditionals-practice.js
+++ conditionals-practice.js
@@ -5,6 +5,37 @@ if(userNum == ourNum) {
   console.log("You guessed it! Lucky!");
 } else if(userNum < ourNum){
   console.log("Sorry, your guess is too low!");
+  if(userNum - ourNum >= -3 && userNum - ourNum <= 3) {
+    console.log("You're warm! Try again!");
+  } else {
+    console.log("You're cold! Try again!");
+  }
 } else {
   console.log("Sorry, your guess is too high!");
+  if(userNum - ourNum >= -3 && userNum - ourNum <= 3) {
+    console.log("You're warm! Try again!");
+  } else {
+    console.log("You're cold! Try again!");
+  }
 }
+
+/*
+var readline = require("readline-sync");
+console.log("Welcome to our phone store!");
+var pricePoint = readline.question("Please enter your price point: ");
+if(pricePoint >= 30) {
+  console.log("You can buy a prepaid phone.");
+  if(pricePoint >= 100) {
+    console.log("You can buy a bottom-tier phone.");
+    if(pricePoint >= 300) {
+      console.log("You can buy a middle-tier phone.");
+      if(pricePoint >= 600) {
+        console.log("You can buy a top-tier phone.");
+      }
+    }
+  }
+}
+else {
+  console.log("Looks like you can't afford any of our phones. Sorry!");
+}
+*/
--- dragon-realm.js
+++ dragon-realm.js
@@ -1,36 +1,27 @@
-/*********************************************
-Attribution: This is script was adapted from the original Python version
-by Al Sweigart in his free online textbook "Invent Your Own Computer Games With Python"
-Source: http://inventwithpython.com/invent4thed/chapter5.html
-*********************************************/
-
 // Author: FirstName LastName
 var readline = require("readline-sync");
 var sleep = require("system-sleep/module");
 
 function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  return parseInt(Math.random() * (max - min) + min);
 }
 
-function displayIntro() {
-  console.log(
-    "You are in a land full of dragons. In front of you, you see\n" +
-    "two caves. In one cave, the dragon is friendly and will share\n" +
-    "his treasure with you. The other dragon is greedy and hungry, and\n" +
-    "will eat you on sight."
-  );
-  console.log();
+// Display intro
+console.log();
+console.log("*****************************************************************");
+console.log("                          DRAGON REALM");
+console.log("                 A game by FirstName LastName");
+console.log("*****************************************************************");
+console.log("You are in a land full of dragons. In front of you, you see");
+console.log("two caves. In one cave, the dragon is friendly and will share");
+console.log("his treasure with you. The other dragon is greedy and hungry");
+console.log("and will eat you on sight.");
+var chosenCave = readline.question("Which cave will you go into? (1 or 2): ");
+if(chosenCave != 1 && chosenCave != 2) {
+  console.log("You chose to go home instead!");
 }
-
-function chooseCave() {
-  var cave = 0;
-  while(cave != 1 && cave != 2) {
-    cave = readline.question("Which cave will you go into? (1 or 2): ");
-  }
-  return cave;
-}
-
-function checkCave(chosenCave) {
+else {
+  var friendlyCave = getRandomNumber(1, 3);
   console.log("You approach the cave...");
   sleep(2000);
   console.log("It is dark and spooky...");
@@ -38,8 +29,6 @@ function checkCave(chosenCave) {
   console.log("A large dragon jumps out in front of you! He opens his jaws and...");
   console.log();
   sleep(2000);
-  var friendlyCave = parseInt(getRandomNumber(1, 3));
-  console.log(friendlyCave);
   if(chosenCave == friendlyCave) {
     console.log("...gives you his treasure!");
   }
@@ -47,15 +36,4 @@ function checkCave(chosenCave) {
     console.log("...gobbles you down in one bite!");
   }
 }
-
-function play() {
-  var playAgain = "yes";
-  while(playAgain == "yes" || playAgain == "y") {
-    displayIntro();
-    var caveNumber = chooseCave();
-    checkCave(caveNumber);
-    playAgain = readline.question("Do you want to play again? (yes or no): ");
-  }
-}
-
-play();
+console.log("Thanks for playing! Goodbye!");
